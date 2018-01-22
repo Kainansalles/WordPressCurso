2120ba12c3dba64f2c4608901bc840cc54317993
Kainan Salles <kainansalles@outlook.com>
2018-01-21 22:05:34 -0200

    Visto em aula: Theme_support - Sidebar - WP-Query

--- index.php
+++ index.php
@@ -1,34 +1,40 @@
 <?php get_header(); ?>
+
+<img class="img-responsive" src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
+
 <div class="conteudo">
 	<main>
-		<section class="slide container">Slide</section>
-		<section class="servicos container">Serviços</section>
-		<section class="meio container">
-			<aside class="barra-lateral col-md-3">Barra Lateral</aside>
-			<div class="noticias col-md-9">
-				<?php // se houver algum post
-					if(have_posts()):
-						// enquanto houver algum post, chamme o post de determinada maneira
-						while(have_posts()):
-							the_post(); 
-					?>											
-						<h1><?php the_title(); ?></h1>
-						<p>Publicado em <?php echo get_the_date(); ?> por <?php the_author(); ?></p>
-						<p>Categorias: <?php the_category(' '); ?></p>
-						<p>Tags: <?php the_tags('Tags: ', ', '); ?></p>
-						<p><?php the_content(); ?></p>
-				<?php 
-				endwhile;
-				else:					
-				?>
-					<p>Não tem nada pra mostrar</p>
-				<?php 
-				endif;
-				?>
+		<section class="meio">
+			<div class="container">
+				<div class="row">
+
+					<div class="noticias col-md-9">
+						<?php 
+							// Se houver algum post
+							if(have_posts()) :
+								// Enquanto houver algum post, chame o post de determinada maneira
+								while (have_posts()) : the_post(); 
+						?>
+
+						<?php get_template_part('content', get_post_format()); ?>
+
+						<?php 
+						endwhile;
+						else:
+						 ?>
+							<p>Nao tem nada ainda pra mostrar</p>
+						<?php 
+						endif;
+						?>
+
+					</div>
+					<aside class="barra-lateral col-md-3">
+						<?php get_sidebar('blog'); ?>
+					</aside>
+				</div>
 			</div>
 		</section>
-		<section class="mapa container">Mapa</section>
-	</main>
-</div>
 
+	</main>	
+</div>
 <?php get_footer(); ?>
--- paginageral.php
+++ paginageral.php
@@ -1,32 +1,39 @@
 <?php 
 /*
-Template Name: Paginas Gerais
+Template Name: Páginas Gerais
 */
  ?>
 
  <?php get_header(); ?>
+
+ <img class="img-responsive" src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
+
 <div class="conteudo-wrapper">
 	<main>
-		<div class="conteudo container">
-			<?php // se houver algum post
-						if(have_posts()):
-							// enquanto houver algum post, chamme o post de determinada maneira
-							while(have_posts()):
-								the_post(); 
-						?>											
-							<h1><?php the_title(); ?></h1>
-							<p>Author: <?php the_author(); ?></p>
-							<p><?php the_content(); ?></p>
-					<?php 
-					endwhile;
-					else:					
-					?>
-						<p>Não tem nada pra mostrar</p>
-					<?php 
-					endif;
-					?>
-		</div>
-	</main>
-</div>
 
+			<div class="conteudo">
+				<div class="container">
+						<?php 
+							// Se houver algum post
+							if(have_posts()) :
+								// Enquanto houver algum post, chame o post de determinada maneira
+								while (have_posts()) : the_post();
+						?>
+							<h1><?php the_title(); ?></h1>
+							<p>Autor: <?php the_author(); ?></p>
+							<p><?php the_content(); ?></p>
+						<?php 
+						endwhile;
+						else:
+						 ?>
+							<p>Nao tem nada ainda pra mostrar</p>
+						<?php 
+						endif;
+						?>					
+
+				</div>
+			</div>
+
+	</main>	
+</div>
 <?php get_footer(); ?>
